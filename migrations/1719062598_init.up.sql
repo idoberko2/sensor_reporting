d4b3e7be3aba4bfe1ae87411c9e5b43642a843d9
Ido Berkovich <idoberko2@gmail.com>
2024-06-22 21:05:30 +0300

    fix test

--- migrations/1719062598_init.up.sql
+++ migrations/1719062598_init.up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE sensors_data (
-    t               TIMESTAMPTZ NOT NULL PRIMARY KEY,
-    sensor          VARCHAR NOT NULL,
-    value           FLOAT NOT NULL
+    t               TIMESTAMPTZ NOT NULL,
+    sensor          VARCHAR(16) NOT NULL,
+    value           FLOAT NOT NULL,
+    CONSTRAINT sensors_data_pkey PRIMARY KEY (t, sensor)
 );
 SELECT create_hypertable('sensors_data', 't');
